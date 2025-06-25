3d8a05b5351122f5cf110c73520a063bf1a5c6f0
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-06-25 21:06:32 +0530

    Added workSpace service

---
+++ backend/src/services/workspaceService.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+import workSpaceRepository from "../repositories/workspaceRepository.js"
+
+export const createWorkSpaceService = async (workSpaceData) => { // this workSpaceData doesn't consist of joinCode becoz joinCode is automatically generated not by any client or server so we have to create a joinCode and check whether is any workSpace have same joinCode or not
+  const joinCode = uuidv4().substring(0 , 6);
+
+  const workSpace = await workSpaceRepository.create({ // when the workSpace is created there should be atleast 1 channel in it + we should add the user as the current admin
+    name: workSpaceData.name,
+    discription: workSpaceData.discription,
+    joinCode
+  }); 
+
+  await workSpaceRepository.addMembertoWorkspace(
+    response._id,
+    workSpaceData.owner,
+    'admin'
+  );
+
+  await workSpaceRepository.addChanneltoWorkspace(
+    response._id,
+    'general'
+  )
+
+  return workSpace;
+}
